278d9337dbeae61ba5c9c8e31dabcbd03b41912c
Vladimir Dubovik <dubo@sfedu.ru>
2022-03-14 17:13:31 +0300

    create first two screens

---
+++ urna/Controller/LoginViewController.swift
@@ -0,0 +1,36 @@
+//
+//  LoginViewController.swift
+//  urna
+//
+//  Created by imac44 on 14.03.2022.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBAction func login(){
+        
+    }
+    @IBAction func registration() {
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
