00971a6910510bfe951de3362935a3b59c533e70
Vladimir Dubovik <dubo@sfedu.ru>
2022-03-14 15:37:03 +0300

    Initial Commit

---
+++ urna/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  urna
+//
+//  Created by imac44 on 14.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
