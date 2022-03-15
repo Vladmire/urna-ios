ab203bd867d9652b94d7615a2c1a601ec53244a4
Vladimir Dubovik <dubo@sfedu.ru>
2022-03-15 17:36:10 +0300

    create a login and register screens

---
+++ urna/Model/Points.swift
@@ -0,0 +1,15 @@
+//
+//  Points.swift
+//  urna
+//
+//  Created by imac44 on 15.03.2022.
+//
+
+import Foundation
+
+struct Points {
+    var name: String = ""
+    var type: String = ""
+    var location: String = ""
+    var image: String = ""
+}
